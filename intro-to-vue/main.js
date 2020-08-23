2005b2c2de790375d1a8c38f428af4de2c5dd99b
paulstey <paulstey@gmail.com>
2020-08-23 12:52:05 -0400

    fixed vue with missing comma

--- intro-to-vue/main.js
+++ intro-to-vue/main.js
@@ -3,7 +3,7 @@ var app = new Vue({
   data: {
   	product: 'Socks',
   	image: './assets/vmSocks-green-onWhite.jpg',
-  	inventory: 8
+  	inventory: 8,
     details: ["80% cotton", "20% polyester", "Gender-neutral"]
   }
 })
