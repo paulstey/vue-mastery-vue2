6d83abbe465b54ebdd708b270d2dc246e5432b0b
paulstey <paulstey@gmail.com>
2020-08-23 13:15:56 -0400

    add mouseover for new product color

--- intro-to-vue/main.js
+++ intro-to-vue/main.js
@@ -4,6 +4,29 @@ var app = new Vue({
   	product: 'Socks',
   	image: './assets/vmSocks-green-onWhite.jpg',
   	inventory: 8,
-    details: ["80% cotton", "20% polyester", "Gender-neutral"]
+    details: ["80% cotton", "20% polyester", "Gender-neutral"],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: 'green',
+        variantImage: './assets/vmSocks-green-onWhite.jpg'
+      },
+      {
+        variantId: 2235,
+        variantColor: 'blue',
+        variantImage: './assets/vmSocks-blue-onWhite.jpg'
+      }
+    ],
+    cart: 0
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1
+    },
+
+    updateProduct(variantImage) {
+      this.image = variantImage
+    }
   }
+
 })
